93deeadb9cb1f09890340af80384cc52495b0ceb
Mika Le Gall <mika.legall@live.fi>
2017-12-11 16:33:13 +0200

    lisätty paikallinen mysli tietokannaksi

--- src/main/java/fi/academy/codeliveserver/Document.java
+++ src/main/java/fi/academy/codeliveserver/Document.java
@@ -6,41 +6,59 @@ package fi.academy.codeliveserver;
 
 
 // MIKA: Muokattu application.properties tiedostoa eli paikallinen SQL-kanta "editorlive" (create database editorlive;)
+// MIKA: Käytetään toistaiseksi JPA (Java Persistence API), kunnes saadaan muunnettua Hibenate natiiviksi
 import javax.persistence.*;
 
-// MIKA: Alla oleva käyttää toistaiseksi JPA (Java Persistence API), kunnes saadaan muunnettua Hibenate natiiviksi
 @Entity
 /*
-Tämä ilmoittaa Hibernatelle, että tee tästä Java-luokasta tietokantataulu. Tässä Java-luokassa on oltava julkinen tai
+MIKA: @Entity ilmoittaa Hibernatelle, että tee tästä Java-luokasta tietokantataulu. Tässä Java-luokassa on oltava julkinen tai
 protected-näkyvyydelle määritelty parametriton konstruktori (oletuskonstruktori riittää),
 tämä luokka ei voi olla final tai enum (etukäteen määritellyt tyypit omaava), tälle luokalle täytyy luoda getterit ja setterit
 */
+
+@Access(AccessType.PROPERTY)
+/*
+MIKA: @Access(AccessType.PROPERTY) muuttaa Java-luokan ja tietokantataulun mäppäyksen
+tehtäväksi gettereiden perusteella eikä jäsenmuuttujien. Tämän vuoksi annotaatiota @Id ja sen mukana olevaa
+tietoa yksilöllisestä avaimesta @GeneratedValue(strategy= GenerationType.AUTO)
+ei sidota jäsenmuuttujaan vaan getteriin
+*/
+
 
 // MIKA: Tässä voidaan antaa tietokantataulun nimi mikäli luokan nimi on eroava eli halutaan käyttää aliasta
 @Table(name = "ASIAKIRJA") // Hibernate natiivilla ilman nimeämistä oletusarvoinen taulu luotaisiin nimellä EVENT
 public class Document {
 
-    @Id // MIKA: Tämä määrittää entiteetin identiteetin yhdeksi kentäksi
-//    @GeneratedValue(generator = "increment") // Hibernate natiivilla + alla oleva annotaatio
-//    @GenericGenerator(name = "increment", strategy = "increment") // Hibernate natiivilla
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    // Tästä on poistettu annotattio @Id, koska @Access(AccessType.PROPERTY)
     private int id;
 
+    @Transient// MIKA: Pyrkimys tulkita tietokantaan tallennettava teksti muodossa String eikä StringBuilder (toimii 11.12.2017 ilman tätäkin).
+    private StringBuilder stringBuilder;
+    private String text;
+    private String filename;
+
+
+    public Document() {
+        this.stringBuilder = new StringBuilder();
+    }
+
+    @Lob // MIKA: String on oletusarvoisesti VARCHAR(255), lisäämällä annotaation @Lob tulee tyypiksi TEXT (voi tosin riippua kannasta!)
+    public String getText() {
+        return stringBuilder.toString();
+    }
+
+    public void setText(String text) {
+        this.stringBuilder = new StringBuilder(text);
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    @Transient
-    private StringBuilder text;
-
-    private String filename;
-
-    public Document() {
-        this.text = new StringBuilder();
     }
 
     public String getFilename() {
@@ -51,21 +69,13 @@ public class Document {
         this.filename = filename;
     }
 
-    @Lob // MIKA: String on oletusarvoisesti VARCHAR(255), lisäämällä annotaation @Lob tulee tyypiksi TEXT (voi tosin riippua kannasta!)
-    public String getText() {
-        return text.toString();
-    }
-
-    public void setText(String text) {
-        this.text = new StringBuilder(text);
-    }
 
     public void delete(int start, int end) {
-        text.delete(start, end);
+        stringBuilder.delete(start, end);
     }
 
     public void insert(int offset, String insertText) {
-        text.insert(offset, insertText);
+        stringBuilder.insert(offset, insertText);
     }
 
     public void insert(int start, int end, String insertText) {
--- src/main/java/fi/academy/codeliveserver/DocumentRepository.java
+++ src/main/java/fi/academy/codeliveserver/DocumentRepository.java
@@ -1,8 +1,7 @@
 package fi.academy.codeliveserver;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
 
-public interface DocumentRepository extends CrudRepository<Document, Integer> {
-
-
+public interface DocumentRepository extends JpaRepository<Document, Integer> {
 }
